a6d70ae56fd87f235edae363cb11dc3e1d501ded
mario2788 <maacuellarma@unal.edu.co>
2022-05-14 14:38:08 -0500

    consolidado

---
+++ parametros.py
@@ -0,0 +1,11 @@
+# Parametros de diseño y operación
+
+
+vel_angular = 2500 # rmp
+diam_int = 30 # mm
+diam_ext = 80 # mm
+
+def data() :
+	print("Parametros llamados")
+	return [vel_angular, diam_int, diam_ext]	
+	
